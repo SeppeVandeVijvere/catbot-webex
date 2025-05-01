682bf944548988b75f4e3afeed7c50ec151033b9
Seppe.Vande.Vijvere <seppe.vande.vijvere@student.howest.be>
2025-05-01 14:26:16 +0200

    Refactor: Alles herbekeken, werkend na splitsing

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from webex_api import *
 from cat_api import *
 from rich import print
+from cat_api import stuur_kat, stuur_rassen, stuur_info
+from webex_api import check_connection, zoek_of_maak_room, laatste_bericht, haal_breed_id
 
 # API keys
 webex_key = input("Geef je Webex API Key in: ")
@@ -12,20 +14,43 @@ room_id = zoek_of_maak_room(input("Room naam: "), webex_headers)
 laatste_id = ""
 
 print("[cyan]Starten met luisteren...[/cyan]")
+
+prefix = "%"
+
 while True:
-    m = laatste_bericht(room_id, webex_headers)
-    if m and m["id"] != laatste_id and m["text"].startswith("%"):
+    m = laatste_bericht(room_id)
+    if m and m["id"] != laatste_id and m["text"].startswith(prefix):
         parts = m["text"].split(" ", 1)
-        cmd = parts[0][1:]
+        cmd = parts[0][1:]  # Verwijdert de % van het commando
         arg = parts[1] if len(parts) > 1 else None
+        
         if cmd == "cat":
-            id = haal_breed_id(arg, cat_headers) if arg else None
-            stuur_kat(room_id, m["id"], webex_headers, cat_headers, "jpg", id)
+            id = haal_breed_id(arg) if arg else None
+            stuur_kat(room_id, m["id"], "jpg", id)
         elif cmd == "gif":
-            id = haal_breed_id(arg, cat_headers) if arg else None
-            stuur_kat(room_id, m["id"], webex_headers, cat_headers, "gif", id)
-        elif cmd == "breeds" or cmd == "rassen":
-            stuur_rassen(room_id, m["id"], webex_headers, cat_headers)
+            id = haal_breed_id(arg) if arg else None
+            stuur_kat(room_id, m["id"], "gif", id)
+        elif cmd == "breeds":
+            stuur_rassen(room_id, m["id"])
         elif cmd == "info" and arg:
-            stuur_info(room_id, m["id"], arg, webex_headers, cat_headers)
+            stuur_info(room_id, m["id"], arg)
+        
         laatste_id = m["id"]
+
+# while True:
+#     m = laatste_bericht(room_id, webex_headers)
+#     if m and m["id"] != laatste_id and m["text"].startswith("%"):
+#         parts = m["text"].split(" ", 1)
+#         cmd = parts[0][1:]
+#         arg = parts[1] if len(parts) > 1 else None
+#         if cmd == "cat":
+#             id = haal_breed_id(arg, cat_headers) if arg else None
+#             stuur_kat(room_id, m["id"], webex_headers, cat_headers, "jpg", id)
+#         elif cmd == "gif":
+#             id = haal_breed_id(arg, cat_headers) if arg else None
+#             stuur_kat(room_id, m["id"], webex_headers, cat_headers, "gif", id)
+#         elif cmd == "breeds" or cmd == "rassen":
+#             stuur_rassen(room_id, m["id"], webex_headers, cat_headers)
+#         elif cmd == "info" and arg:
+#             stuur_info(room_id, m["id"], arg, webex_headers, cat_headers)
+#         laatste_id = m["id"]
