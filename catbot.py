c3d2a4c50ba3df2c77bbae4b0eba68573620f990
Seppe.Vande.Vijvere <seppe.vande.vijvere@student.howest.be>
2025-05-01 12:40:39 +0200

    Added: check_connection functie om API-conecties te testen

--- catbot.py
+++ catbot.py
@@ -10,3 +10,13 @@ cat_headers = {"x-api-key": "live_EZYC6zWcD75xxtCBwUbGZUTUsi3JCctk4us9NzaIyZTP3q
 webex_url = "https://webexapis.com/v1/"
 cat_url = "https://api.thecatapi.com/v1/"
 prefix = "%"
+
+# Hulpjes
+def check_connection():
+    webex_ok = requests.get(webex_url + "rooms", headers=webex_headers)
+    cat_ok = requests.get(cat_url + "images", headers=cat_headers)
+    if webex_ok.status_code == 200 and cat_ok.status_code == 200:
+        print("[green]Connectie geslaagd[/green]")
+    else:
+        print("[red]Probleem met verbinding.[/red]")
+        exit()
